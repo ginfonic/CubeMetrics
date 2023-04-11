7524e3e0d4548f2a4c4340deebf07d52123a4fdf
Evgeny Fokin <ginfonic@gmail.com>
2023-04-11 09:26:23 +0300

    1.0

---
+++ csv2xlsx.ps1
@@ -0,0 +1,26 @@
+#Define locations and delimiter
+$csv = "C:\Users\fokin-evga\Documents\Development\CubeMetrics\data\cbm_out_15.csv" #Location of the source file
+$xlsx = "C:\Users\fokin-evga\Documents\Development\CubeMetrics\data\cbm_out_15.xlsx" #Desired location of output
+$delimiter = "," #Specify the delimiter used in the file
+
+# Create a new Excel workbook with one empty sheet
+$excel = New-Object -ComObject excel.application 
+$workbook = $excel.Workbooks.Add(1)
+$worksheet = $workbook.worksheets.Item(1)
+
+# Build the QueryTables.Add command and reformat the data
+$TxtConnector = ("TEXT;" + $csv)
+$Connector = $worksheet.QueryTables.add($TxtConnector,$worksheet.Range("A1"))
+$query = $worksheet.QueryTables.item($Connector.name)
+$query.TextFileOtherDelimiter = $delimiter
+$query.TextFileParseType  = 1
+$query.TextFileColumnDataTypes = ,1 * $worksheet.Cells.Columns.Count
+$query.AdjustColumnWidth = 1
+
+# Execute & delete the import query
+$query.Refresh()
+$query.Delete()
+
+# Save & close the Workbook as XLSX.
+$Workbook.SaveAs($xlsx,51)
+$excel.Quit()
